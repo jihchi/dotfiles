6816a15097bf3d5adbfe7c74d0b973aee97a422e
Archie Lee <archielee@appier.com>
2017-09-10 15:50:27 +0800

    Add bash_it custom files

---
+++ .bash_it/aliases/custom.aliases.bash
@@ -0,0 +1,40 @@
+# npm-quick-run
+function __nr() {
+  executor="`npm bin -g`/nr"
+
+  if [[ -z "$@" ]]; then
+    "$executor" -i
+  else
+    "$executor" $@
+  fi
+}
+
+alias nr="__nr"
+
+# general
+alias cdc="cd ~ && clear"
+
+# svgo
+alias svgo="svgo --config=$HOME/.svgo.yaml"
+
+# Sublime Text
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+
+# gb
+alias gogb="$GOPATH/bin/gb"
+
+# git
+alias gdt="git difftool"
+alias gdtc="git difftool --cached"
+alias gmt="git mergetool"
+alias rmorig="find . -name \"*.orig\" ! -path \"./node_modules/*\" | xargs -I {} rm -v \"{}\""
+alias rmswp="find . -name \"*.swp\"| xargs rm -v"
+alias gub='git remote update ${GIT_REMOTE:-origin} --prune'
+alias gd="git dsf"
+alias gfp="git checkout develop && git push && git checkout master && git push && git push --tag"
+alias gcom="git checkout master"
+alias gcop="git checkout develop"
+
+# silver searcher
+alias search="ag --literal --hidden --filename-pattern"
+
---
+++ .bash_it/lib/custom.bash
@@ -0,0 +1,41 @@
+# Homebrew
+export PATH="/usr/local/sbin:$PATH"
+
+# GVM
+[[ -s "/archie/.gvm/scripts/gvm" ]] && source "/archie/.gvm/scripts/gvm"
+
+
+# locale
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# editor
+export EDITOR=vim
+
+# fzf
+# Use ~~ as the trigger sequence instead of the default **
+export FZF_COMPLETION_TRIGGER='~~'
+
+# There are also _fzf_path_completion and _fzf_dir_completion
+complete -F _fzf_file_completion -o default -o bashdefault doge
+
+_fzf_compgen_path() {
+  ag -g "" "$1"
+}
+
+# Setting ag as the default source for fzf
+export FZF_DEFAULT_COMMAND='ag -g ""'
+
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# iTerm2 shell integration
+test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+
+# OPAM
+eval $(opam config env)
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
