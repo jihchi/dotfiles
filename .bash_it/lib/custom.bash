f06bfed1957c05594c2858ca2a934d7f68c38a57
Archie Lee <archielee@appier.com>
2017-10-27 13:43:43 +0800

    Re-format custom.bash

--- .bash_it/lib/custom.bash
+++ .bash_it/lib/custom.bash
@@ -4,7 +4,6 @@ export PATH="/usr/local/sbin:$PATH"
 # GVM
 [[ -s "/archie/.gvm/scripts/gvm" ]] && source "/archie/.gvm/scripts/gvm"
 
-
 # locale
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -38,4 +37,4 @@ export NVM_DIR="$HOME/.nvm"
 
 # fasd
 eval "$(fasd --init auto)"
-:
+
