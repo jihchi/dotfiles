510ab4fb8d2afb7c053f768ba4a39dbdcc590a0e
Archie Lee <archielee@appier.com>
2017-11-16 15:22:29 +0800

    Unalias before define

--- .bash_it/plugins/custom.plugins.bash
+++ .bash_it/plugins/custom.plugins.bash
@@ -1,4 +1,5 @@
 # fasd & fzf change directory - open best matched file using `fasd` if given argument, filter output of `fasd` using `fzf` else
+unalias v
 v() {
   [ $# -gt 0 ] && fasd -f -e ${EDITOR} "$*" && return
   local file
@@ -6,6 +7,7 @@ v() {
 }
 
 # fasd & fzf change directory - jump using `fasd` if given argument, filter output of `fasd` using `fzf` else
+unalias z
 z() {
   [ $# -gt 0 ] && fasd_cd -d "$*" && return
   local dir
