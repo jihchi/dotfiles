ae4993acfb0d352cb319442dfba70bab4d45caf8
Archie Lee <archielee@appier.com>
2018-01-23 10:07:56 +0800

    Add nps completion script

--- .bash_it/lib/custom.bash
+++ .bash_it/lib/custom.bash
@@ -32,3 +32,32 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
+###-begin-nps-completions-###
+#
+# yargs command completion script
+#
+# Installation: nps completion >> ~/.bashrc
+#    or nps completion >> ~/.bash_profile on OSX.
+#
+_yargs_completions()
+{
+    local cur_word args type_list
+
+    cur_word="${COMP_WORDS[COMP_CWORD]}"
+    args=("${COMP_WORDS[@]}")
+
+    # ask yargs to generate completions.
+    type_list=$(nps --get-yargs-completions "${args[@]}")
+
+    COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
+
+    # if no match was found, fall back to filename completion
+    if [ ${#COMPREPLY[@]} -eq 0 ]; then
+      COMPREPLY=( $(compgen -f -- "${cur_word}" ) )
+    fi
+
+    return 0
+}
+complete -F _yargs_completions nps
+###-end-nps-completions-###
+
