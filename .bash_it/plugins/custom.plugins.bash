8268996c8a986f855e4721bd64e96675053cbf68
Archie Lee <archielee@appier.com>
2018-06-21 22:00:51 +0800

    Add transfer command

--- .bash_it/plugins/custom.plugins.bash
+++ .bash_it/plugins/custom.plugins.bash
@@ -13,3 +13,14 @@ z() {
   local dir
   dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
 }
+
+# https://github.com/dutchcoders/transfer.sh/
+transfer() {
+    # write to output to tmpfile because of progress bar
+    tmpfile=$( mktemp -t transferXXX )
+    curl --progress-bar --upload-file $1 https://transfer.sh/$(basename $1) >> $tmpfile;
+    cat $tmpfile;
+    rm -f $tmpfile;
+}
+
+alias transfer=transfer
