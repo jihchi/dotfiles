612d0f74ee34942b4196c89133d84514771d5936
Archie Lee <archielee@appier.com>
2018-08-09 21:56:43 +0800

    Cleanup custom.aliases.bash

--- .bash_it/aliases/custom.aliases.bash
+++ .bash_it/aliases/custom.aliases.bash
@@ -1,44 +1,16 @@
-alias gotest="fswatch -o . | xargs -n1 -I% time go test -v ./..."
-alias curl_json="curl -H'Content-Type: application/json'"
-alias curl_form="curl -H'Content-Type: application/x-www-form-urlencoded'"
-alias curl_data="curl -H'Content-Type: multipart/form-data'"
+alias watch="fswatch -o . | xargs -n1 -I%"
 alias dl="aria2c --max-concurrent-downloads=50 --continue=true --max-connection-per-server=16 --min-split-size=10M --split=50"
-
-# npm-quick-run
-function __nr() {
-  executor="`npm bin -g`/nr"
-
-  if [[ -z "$@" ]]; then
-    "$executor" -i
-  else
-    "$executor" $@
-  fi
-}
-
-alias nr="__nr"
 
 # general
 alias cdc="cd ~ && clear"
+alias rmorig="ag -i -g \"\.orig$\" | xargs -I _ rm -v \"_\""
+alias rmswp="ag -i -g \"\.swp$\" | xargs -I _ rm -v \"_\""
 
 # Sublime Text
 alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
-
-# gb
-alias gogb="$GOPATH/bin/gb"
 
 # git
 alias gdt="git difftool"
 alias gdtc="git difftool --cached"
 alias gmt="git mergetool"
-alias rmorig="ag -i -g \"\.orig$\" | xargs -I _ rm -v \"_\""
-alias rmswp="ag -i -g \"\.swp$\" | xargs -I _ rm -v \"_\""
-alias gub='git remote update ${GIT_REMOTE:-origin} --prune'
-alias gd="git dsf"
-alias gfp="git checkout develop && git push && git checkout master && git push && git push --tag"
-alias gcom="git checkout master"
-alias gcop="git checkout develop"
-
-# silver searcher
-alias search="ag --literal --hidden"
-alias ff="search --filename-pattern"
 
