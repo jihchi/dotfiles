5b03d182787a2334d197a570bf62d36072f6bce4
jihchi <achi@987.tw>
2019-03-05 22:05:32 +0800

    Clean up bash-it customization

--- .bash_it/aliases/custom.aliases.bash
+++ .bash_it/aliases/custom.aliases.bash
@@ -1,14 +1,7 @@
-alias watch="fswatch -o . | xargs -n1 -I%"
-alias dl="aria2c --max-concurrent-downloads=50 --continue=true --max-connection-per-server=16 --min-split-size=10M --split=50"
-
 # general
 alias cdc="cd ~ && clear"
 alias rmorig="ag -i -g \"\.orig$\" | xargs -I _ rm -v \"_\""
 alias rmswp="ag -i -g \"\.swp$\" | xargs -I _ rm -v \"_\""
-
-# Sublime Text
-alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
-
 # git
 alias gdt="git difftool"
 alias gdtc="git difftool --cached"
--- .bash_it/lib/custom.bash
+++ .bash_it/lib/custom.bash
@@ -1,13 +1,5 @@
 # locale
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-
 # editor
 export EDITOR=vim
-
-# Go Version Manager
-[[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
-
-# fnm
-export PATH=$HOME/.fnm:$PATH
-[[ -s "$HOME/.fnm/fnm" ]] && eval `fnm env`
