46bf1117bba5b07276f916e4016babd084eb6314
jihchi <achi@987.tw>
2020-11-04 20:04:37 +0800

    Remove `.bash_it` folder in favor of zsh

--- .bash_it/lib/custom.bash
+++
@@ -1,8 +0,0 @@
-# locale
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-# editor
-export EDITOR=vim
-# fzf
-export FZF_DEFAULT_COMMAND='ag -g ""'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
